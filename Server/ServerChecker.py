6ada6807c2e86e72329983d6eeb68f1ea1c868c4
Yair.Bloom <Yair.Bloom@xjet3d.com>
2021-06-28 14:24:16 +0300

    Insert tests Insert printer + upload job

--- Server/ServerChecker.py
+++ Server/ServerChecker.py
@@ -1,4 +1,7 @@
 import pymongo
+import requests
+import time
+import os.path
 
 
 class ServerChecker:
@@ -7,6 +10,55 @@ class ServerChecker:
     NanoDB = MongoClient["NanoDB"]
 
     self.PrintersCollection = NanoDB["printers"]
+    self.BasicUrl = 'http://localhost:3000/' 
+    self.InsertPrinterUrl = self.BasicUrl + 'printers/AddPrinter'
+    self.NewJobUrl = self.BasicUrl + 'printers/NewJob'
+    self.GetJobInfoUrl = self.BasicUrl + 'printers/GetJobInfo'
+    self.PrinterName = f'{time.time()}-Printer'
+    self.FileToUpload = os.path.join(os.path.dirname(os.path.realpath(__file__)),'Reference' , 'DC to DC up converter.pcbjc')
+    self.PrinterDBItem = None
+
+#########################################################################################################
+
+  def CheckConnection(self):
+    res = requests.get(self.BasicUrl)
+    return (res.status_code==200)
+
+#########################################################################################################
+  def InsertPrinter(self):
+    #Sending insert printer request to the server 
+    res = requests.post(self.InsertPrinterUrl, 
+                        data={'PrinterName':self.PrinterName,'UniqueString':self.PrinterName})
+    if res.status_code!=200:
+      return False
+
+    #Checking it is exist in the DB
+    self.PrinterDBItem = self.PrintersCollection.find_one({'UniqueString':self.PrinterName})
+    print(self.PrinterDBItem)
+
+    return self.PrinterDBItem!= None
+
+#########################################################################################################
+  def UploadJob(self):
+    print(self.FileToUpload)
+    with open(self.FileToUpload, 'rb') as f:
+      files = {'myFile': (os.path.basename(self.FileToUpload), f, 'application/gzip')}
+      data = {'JobName': os.path.basename(self.FileToUpload), 'UniqueString':self.PrinterName}
+      res = requests.post(self.NewJobUrl, files=files, data=data)
+      if not res or res.status_code!=200:
+        return False
+      JsonRsp = res.json()
+      if not JsonRsp:
+        return False
+      self.PrinterDBItem = self.PrintersCollection.find_one({'UniqueString':self.PrinterName})
+      for JobItem in self.PrinterDBItem['Jobs']:
+        if (JobItem['JobPath'] == JsonRsp['JobPath'] and 
+            os.path.isfile(JobItem['JobPath']) and 
+            os.path.getsize(JobItem['JobPath']) == os.path.getsize(self.FileToUpload)):
+          return True
+      return False
+
+#########################################################################################################
   def test1(self):
 
     x = self.PrintersCollection.find_one()
@@ -21,6 +73,9 @@ class ServerChecker:
 
 if __name__ == "__main__":
   serverChecker = ServerChecker()
-  serverChecker.test1()
+  print(serverChecker.CheckConnection())
+  print(serverChecker.InsertPrinter())
+  print(serverChecker.UploadJob())
+  #serverChecker.test1()
 
   
